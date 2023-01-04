9f2486a3d313b44f0b5d88be99ecb02486066c46
tincho <tinchoabbate@noreply.users.github.com>
2023-01-04 13:09:15 +0100

    update tokens

--- contracts/DamnValuableToken.sol
+++ contracts/DamnValuableToken.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 /**
  * @title DamnValuableToken
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
-contract DamnValuableToken is ERC20 {
-
-    // Decimals are set to 18 by default in `ERC20`
-    constructor() ERC20("DamnValuableToken", "DVT") {
+contract DamnValuableToken is ERC20Permit {
+    constructor() ERC20("DamnValuableToken", "DVT") ERC20Permit("DamnValuableToken") {
         _mint(msg.sender, type(uint256).max);
     }
 }
