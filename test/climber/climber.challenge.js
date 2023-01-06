f5798753888e37ee94ba795559606c7336e3cdfc
tincho <tinchoabbate@noreply.users.github.com>
2023-01-06 10:59:38 +0100

    update climber

--- test/climber/climber.challenge.js
+++ test/climber/climber.challenge.js
@@ -1,23 +1,20 @@
 const { ethers, upgrades } = require('hardhat');
 const { expect } = require('chai');
+const { setBalance } = require('@nomicfoundation/hardhat-network-helpers');
 
 describe('[Challenge] Climber', function () {
     let deployer, proposer, sweeper, player;
 
-    // Vault starts with 10 million tokens
-    const VAULT_TOKEN_BALANCE = ethers.utils.parseEther('10000000');
+    const VAULT_TOKEN_BALANCE = 10000000n * 10n ** 18n;
+    const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 17n;
+    const TIMELOCK_DELAY = 60 * 60;
 
     before(async function () {
         /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
         [deployer, proposer, sweeper, player] = await ethers.getSigners();
 
-        await ethers.provider.send("hardhat_setBalance", [
-            player.address,
-            "0x16345785d8a0000", // 0.1 ETH
-        ]);
-        expect(
-            await ethers.provider.getBalance(player.address)
-        ).to.equal(ethers.utils.parseEther('0.1'));
+        await setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
+        expect(await ethers.provider.getBalance(player.address)).to.equal(PLAYER_INITIAL_ETH_BALANCE);
         
         // Deploy the vault behind a proxy using the UUPS pattern,
         // passing the necessary addresses for the `ClimberVault::initialize(address,address,address)` function
@@ -28,7 +25,7 @@ describe('[Challenge] Climber', function () {
         );
 
         expect(await this.vault.getSweeper()).to.eq(sweeper.address);
-        expect(await this.vault.getLastWithdrawalTimestamp()).to.be.gt('0');
+        expect(await this.vault.getLastWithdrawalTimestamp()).to.be.gt(0);
         expect(await this.vault.owner()).to.not.eq(ethers.constants.AddressZero);
         expect(await this.vault.owner()).to.not.eq(deployer.address);
         
@@ -38,12 +35,19 @@ describe('[Challenge] Climber', function () {
             await ethers.getContractFactory('ClimberTimelock', deployer)
         ).attach(timelockAddress);
         
+        // Ensure timelock delay is correct and cannot be changed
+        expect(await this.timelock.delay()).to.eq(TIMELOCK_DELAY);
+        await expect(this.timelock.updateDelay(TIMELOCK_DELAY + 1)).to.be.revertedWithCustomError(this.timelock, 'CallerNotTimelock');
+        
         // Ensure timelock roles are correctly initialized
         expect(
-            await this.timelock.hasRole(await this.timelock.PROPOSER_ROLE(), proposer.address)
+            await this.timelock.hasRole(ethers.utils.id("PROPOSER_ROLE"), proposer.address)
         ).to.be.true;
         expect(
-            await this.timelock.hasRole(await this.timelock.ADMIN_ROLE(), deployer.address)
+            await this.timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), deployer.address)
+        ).to.be.true;
+        expect(
+            await this.timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), this.timelock.address)
         ).to.be.true;
 
         // Deploy token and transfer initial token balance to the vault
@@ -51,13 +55,13 @@ describe('[Challenge] Climber', function () {
         await this.token.transfer(this.vault.address, VAULT_TOKEN_BALANCE);
     });
 
-    it('Execution', async function () {        
+    it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
     });
 
     after(async function () {
         /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
-        expect(await this.token.balanceOf(this.vault.address)).to.eq('0');
+        expect(await this.token.balanceOf(this.vault.address)).to.eq(0);
         expect(await this.token.balanceOf(player.address)).to.eq(VAULT_TOKEN_BALANCE);
     });
 });
