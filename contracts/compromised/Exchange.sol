2038f0da1ac4441bcf19c17cb9a3f865bce992b5
tincho <tinchoabbate@noreply.users.github.com>
2023-01-06 20:54:51 +0100

    update compromised

--- contracts/compromised/Exchange.sol
+++ contracts/compromised/Exchange.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 import "./TrustfulOracle.sol";
 import "../DamnValuableNFT.sol";
 
@@ -12,51 +11,62 @@ import "../DamnValuableNFT.sol";
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract Exchange is ReentrancyGuard {
-
     using Address for address payable;
 
     DamnValuableNFT public immutable token;
     TrustfulOracle public immutable oracle;
 
+    error InvalidPayment();
+    error SellerNotOwner(uint256 id);
+    error TransferNotApproved();
+    error NotEnoughFunds();
+
     event TokenBought(address indexed buyer, uint256 tokenId, uint256 price);
     event TokenSold(address indexed seller, uint256 tokenId, uint256 price);
 
-    constructor(address oracleAddress) payable {
+    constructor(address _oracle) payable {
         token = new DamnValuableNFT();
-        oracle = TrustfulOracle(oracleAddress);
+        oracle = TrustfulOracle(_oracle);
     }
 
     function buyOne() external payable nonReentrant returns (uint256) {
-        uint256 amountPaidInWei = msg.value;
-        require(amountPaidInWei > 0, "Amount paid must be greater than zero");
+        uint256 payment = msg.value;
+        if (payment == 0)
+            revert InvalidPayment();
 
         // Price should be in [wei / NFT]
-        uint256 currentPriceInWei = oracle.getMedianPrice(token.symbol());
-        require(amountPaidInWei >= currentPriceInWei, "Amount paid is not enough");
+        uint256 price = oracle.getMedianPrice(token.symbol());
+        if (payment < price)
+            revert InvalidPayment();
 
-        uint256 tokenId = token.safeMint(msg.sender);
-        
-        payable(msg.sender).sendValue(amountPaidInWei - currentPriceInWei);
+        uint256 id = token.safeMint(msg.sender);
+        unchecked {
+            payable(msg.sender).sendValue(payment - price);
+        }
 
-        emit TokenBought(msg.sender, tokenId, currentPriceInWei);
+        emit TokenBought(msg.sender, id, price);
 
-        return tokenId;
+        return id;
     }
 
-    function sellOne(uint256 tokenId) external nonReentrant {
-        require(msg.sender == token.ownerOf(tokenId), "Seller must be the owner");
-        require(token.getApproved(tokenId) == address(this), "Seller must have approved transfer");
+    function sellOne(uint256 id) external nonReentrant {
+        if(msg.sender != token.ownerOf(id))
+            revert SellerNotOwner(id);
+    
+        if(token.getApproved(id) != address(this))
+            revert TransferNotApproved();
 
         // Price should be in [wei / NFT]
-        uint256 currentPriceInWei = oracle.getMedianPrice(token.symbol());
-        require(address(this).balance >= currentPriceInWei, "Not enough ETH in balance");
+        uint256 price = oracle.getMedianPrice(token.symbol());
+        if(address(this).balance < price)
+            revert NotEnoughFunds();
 
-        token.transferFrom(msg.sender, address(this), tokenId);
-        token.burn(tokenId);
-        
-        payable(msg.sender).sendValue(currentPriceInWei);
+        token.transferFrom(msg.sender, address(this), id);
+        token.burn(id);
 
-        emit TokenSold(msg.sender, tokenId, currentPriceInWei);
+        payable(msg.sender).sendValue(price);
+
+        emit TokenSold(msg.sender, id, price);
     }
 
     receive() external payable {}
