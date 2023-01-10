ff4c3031d3d9b9fc9efa63e6ef104ad70bd122ae
tincho <tinchoabbate@noreply.users.github.com>
2023-01-10 11:41:20 +0100

    update free rider

--- contracts/free-rider/FreeRiderBuyer.sol
+++ contracts/free-rider/FreeRiderBuyer.sol
@@ -11,41 +11,50 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract FreeRiderBuyer is ReentrancyGuard, IERC721Receiver {
-
     using Address for address payable;
-    address private immutable partner;
+
+    uint256 private constant PRIZE = 45 ether;
+    address private immutable beneficiary;
     IERC721 private immutable nft;
-    uint256 private constant JOB_PAYOUT = 45 ether;
     uint256 private received;
 
-    constructor(address _partner, address _nft) payable {
-        require(msg.value == JOB_PAYOUT);
-        partner = _partner;
+    error NotEnoughFunding();
+    error CallerNotNFT();
+    error OriginNotBeneficiary();
+    error InvalidTokenID(uint256 tokenId);
+    error StillNotOwningToken(uint256 tokenId);
+
+    constructor(address _beneficiary, address _nft) payable {
+        if (msg.value != PRIZE)
+            revert NotEnoughFunding();
+        beneficiary = _beneficiary;
         nft = IERC721(_nft);
         IERC721(_nft).setApprovalForAll(msg.sender, true);
     }
 
     // Read https://eips.ethereum.org/EIPS/eip-721 for more info on this function
-    function onERC721Received(
-        address,
-        address,
-        uint256 _tokenId,
-        bytes memory
-    ) 
+    function onERC721Received(address, address, uint256 _tokenId, bytes memory _data)
         external
         override
         nonReentrant
-        returns (bytes4) 
+        returns (bytes4)
     {
-        require(msg.sender == address(nft), "Caller is not the NFT contract");
-        require(tx.origin == partner, "Transaction origin is not the partner");
-        require(_tokenId >= 0 && _tokenId <= 5, "Unexpected token ID");
-        require(nft.ownerOf(_tokenId) == address(this), "Still not the owner of the specified token");
-        
-        received++;
-        if(received == 6) {            
-            payable(partner).sendValue(JOB_PAYOUT);
-        }            
+        if (msg.sender != address(nft))
+            revert CallerNotNFT();
+
+        if (tx.origin != beneficiary)
+            revert OriginNotBeneficiary();
+
+        if (_tokenId > 5)
+            revert InvalidTokenID(_tokenId);
+
+        if (nft.ownerOf(_tokenId) != address(this))
+            revert StillNotOwningToken(_tokenId);
+
+        if (++received == 6) {
+            address recipient = abi.decode(_data, (address));
+            payable(recipient).sendValue(PRIZE);
+        }
 
         return IERC721Receiver.onERC721Received.selector;
     }
